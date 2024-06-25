a9bb8f13d7e994244fb9803b002fd075a16228df
vagmr <2737165206@qq.com>
2024-06-25 13:27:59 +0800

    fixed

--- App/router/file.py
+++ App/router/file.py
@@ -80,7 +80,7 @@ def download_file(file_id):
     file = File.query.filter_by(id=file_id, user_id=user_id).first()
     if not file:
         return jsonify({"code": 404, "msg": "文件未找到"}), 404
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file_path = os.path.join("/home/vagmr/var/static/uploads/", file.filename)
     print(file_path, file.filename)
     if os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
